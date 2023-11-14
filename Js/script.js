7a5d407495d80a9c39ee1a74e72ee9e9536c58ff
Kunlek10 <kt445@njit.edu>
2023-11-14 17:51:34 -0500

    change

--- Js/script.js
+++ Js/script.js
@@ -1,2 +1,53 @@
-console.log("Hello Kunlek")
+// Taking fruits to JS
+const fruits = [{
+    image: 'Picture/apple.jpg',
+    name:'Apple',
+    price: 45
+},
+{
+    image: 'Picture/pineapple.jpg',
+    name: 'Pineapple',
+    price: 34
+},
+{
+    image: "Picture/mangos.jpeg",
+    name: "Mango",
+    price: 50
+},
+{
+    image: "Picture/banana.jpg",
+    name: "Banana",
+    price: 25
+}]
+;
 
+let fruits_HTML = '';
+
+fruits.forEach((fruit) => {
+    fruits_HTML += `
+        <section id="fruits" class="fruits">
+                <img src=${fruit.image} height="230" width="230">
+                <table>
+                    <tr>
+                        <td>${fruit.name}</td>
+                    </tr>
+                    <tr>
+                        <td>$${fruit.price}
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>
+                            <button onclick="
+                                {
+                                    add_to_cart
+                                }
+                                ">Add to cart </button>
+                        </td>
+                    </tr>
+                </table>
+            </section>` ;
+
+
+});
+
+console.log(fruits_HTML);
